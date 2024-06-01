7a2b44c9fafe828c83fd7ea2767d8d23693cf935
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-06-01 13:12:39 -0300

    enriquecendo o modelo de dominío parte I

--- CleanArchMVC.Domain/Entities/Category.cs
+++ CleanArchMVC.Domain/Entities/Category.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CleanArchMVC.Domain.Entities
 {
-    internal class Category
+    public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
