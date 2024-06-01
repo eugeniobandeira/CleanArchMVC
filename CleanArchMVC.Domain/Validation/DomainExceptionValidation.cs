4ed1782d28f0c3b93eabb4a9438737db44f6ee57
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-06-01 13:36:42 -0300

    enriquecendo o modelo de domínio parte II

--- CleanArchMVC.Domain/Validation/DomainExceptionValidation.cs
+++ CleanArchMVC.Domain/Validation/DomainExceptionValidation.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CleanArchMVC.Domain.Validation
 {
-    internal class DomainExceptionValidation
+    public class DomainExceptionValidation : Exception
     {
+        public DomainExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+        }
     }
 }
