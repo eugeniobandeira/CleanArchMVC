4ea50353ae9f7cf7c5aa2bf99822b4f2633de144
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-06-03 15:30:15 -0300

    feat: teste de unidade para a entidade Category

--- CleanArchMVC.Domain/Interface/ICategoryRepository.cs
+++ CleanArchMVC.Domain/Interface/ICategoryRepository.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMVC.Domain.Entities;
 
 namespace CleanArchMVC.Domain.Interface
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int? id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> DeleteAsync(Category category);
     }
 }
--- CleanArchMVC.Domain/Interface/IProductRepository.cs
+++ CleanArchMVC.Domain/Interface/IProductRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using CleanArchMVC.Domain.Entities;
 
 namespace CleanArchMVC.Domain.Interface
 {
-    internal interface IProductRepository
+    public interface IProductRepository
     {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> DeleteAsync(Product product);
+
     }
 }
