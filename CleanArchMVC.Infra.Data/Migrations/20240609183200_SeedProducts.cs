1beecb15bd6a73e00ab022058a39a16518a6017d
eugeniobandeira <eugeniobandeira00@gmail.com>
2024-06-09 16:58:13 -0300

    feat: mappings de category e product

---
+++ CleanArchMVC.Infra.Data/Migrations/20240609183200_SeedProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMVC.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Caderno espiral', 'Caderno espiral 100 folhas', 7.45, 50, 'caderno1.jpg', 1)");
+
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Estojo escolar', 'Estojo escolar branco', 2.50, 16, 'estojo.jpg', 1)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
