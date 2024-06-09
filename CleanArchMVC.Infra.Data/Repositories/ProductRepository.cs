9fe2112a504e14d4e4a001a0b338f1eebf907293
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-06-09 14:49:01 -0300

    feat: DependencyInjection

--- CleanArchMVC.Infra.Data/Repositories/ProductRepository.cs
+++ CleanArchMVC.Infra.Data/Repositories/ProductRepository.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Domain.Interface;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMVC.Infra.Data.Repositories
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        ApplicationDbContext _productContext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContext.Products.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> DeleteAsync(Product product)
+        {
+            _productContext.Products.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            //eager loading
+            return await _productContext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
     }
 }
--- CleanArchMVC.Infra.IoC/DependencyInjection.cs
+++ CleanArchMVC.Infra.IoC/DependencyInjection.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMVC.Domain.Interface;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchMVC.Infra.IoC
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrasctructure(
+            this IServiceCollection services,
+            IConfiguration configuration) 
+        { 
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            m => m.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, ICategoryRepository>();
+            services.AddScoped<IProductRepository, IProductRepository>();
+
+            return services;
+        }
     }
 }
